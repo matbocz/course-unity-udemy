60f34994bde438d1203f8b2c6616d136212e8c03
Mateusz Boczarski <matbocz@outlook.com>
2021-02-01 16:00:13 +0100

    Create gizmos for enemy

--- Zombie_Runner/Assets/Scripts/EnemyAI.cs
+++ Zombie_Runner/Assets/Scripts/EnemyAI.cs
@@ -27,4 +27,10 @@ public class EnemyAI : MonoBehaviour
             navMeshAgent.SetDestination(target.position);
         }
     }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
+    }
 }
